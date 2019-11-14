b2801f287cbd192f5a9cb0aacee0bd5a77a97e70
papahance98 <khance@zagmail.gonzaga.edu>
2019-11-13 18:38:23 -0800

    updated stuff

---
+++ prj3.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO manufac_location VALUES(1, "106 E Meme Street", "Spokane", "Washington",
+"United States", "PO BOX 77768");
+INSERT INTO manufac_location VALUES(2, "123 E Boone Avenue", "Seattle","Washington",
+"United States", "PO BOX 6588");
+
+INSERT INTO manufacturer VALUES(1,"Pepsi Co",1,500000000);
+INSERT INTO manufacturer VALUES(2,"Coca-cola Co",2,4500000000);
+
+INSERT INTO flavor VALUES(1,"Cola");
+INSERT INTO flavor VALUES(2,"Lemon-Lime");
+INSERT INTO flavor VALUES(3,"Cherry");
+
+INSERT INTO drink_type VALUES(1,"Soda");
+INSERT INTO drink_type VALUES(2, "Health");
+
+INSERT INTO drink VALUES(1,"Bepsi",1,100,40,25,16,1,1);
+INSERT INTO drink VALUES(2,"Coke",2,100,36,27,16,1,1);
+INSERT INTO drink VALUES(3,"Mountain Loo",1,96,38,25,8,1,2);
+INSERT INTO drink VALUES(4,"Cherry Bepsi",1,100,45,39,12,1,3);
+INSERT INTO drink VALUES(5,"Vitamin Water Cherry",2,0,36,45,16,2,3);
+
+
+
--- soda.sql
+++ soda.sql
@@ -51,3 +51,27 @@ CREATE TABLE drink(
     FOREIGN KEY (drink_type_id) REFERENCES drink_type (drink_type_id),
     FOREIGN KEY (flavor_id) REFERENCES flavor (flavor_id)
 );
+
+INSERT INTO manufac_location VALUES(1, "106 E Meme Street", "Spokane", "Washington",
+"United States", "PO BOX 77768");
+INSERT INTO manufac_location VALUES(2, "123 E Boone Avenue", "Seattle","Washington",
+"United States", "PO BOX 6588");
+
+INSERT INTO manufacturer VALUES(1,"Pepsi Co",1,5000000);
+INSERT INTO manufacturer VALUES(2,"Coca-cola Co",2,45000000);
+
+INSERT INTO flavor VALUES(1,"Cola");
+INSERT INTO flavor VALUES(2,"Lemon-Lime");
+INSERT INTO flavor VALUES(3,"Cherry");
+
+INSERT INTO drink_type VALUES(1,"Soda");
+INSERT INTO drink_type VALUES(2, "Health");
+
+INSERT INTO drink VALUES(1,"Bepsi",1,100,40,25,16,1,1);
+INSERT INTO drink VALUES(2,"Coke",2,100,36,27,16,1,1);
+INSERT INTO drink VALUES(3,"Mountain Loo",1,96,38,25,8,1,2);
+INSERT INTO drink VALUES(4,"Cherry Bepsi",1,100,45,39,12,1,3);
+INSERT INTO drink VALUES(5,"Vitamin Water Cherry",2,0,36,45,16,2,3);
+
+SELECT *
+FROM drink d JOIN flavor f ON 
