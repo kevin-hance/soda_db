c6fef6b5785c63f8a7c501e180011df4f1c20f31
papahance98 <khance@zagmail.gonzaga.edu>
2019-11-13 18:27:07 -0800

    updated stuff

--- soda.sql
+++ soda.sql
@@ -1,10 +1,17 @@
+DROP TABLE IF EXISTS drink;
+DROP TABLE IF EXISTS flavor;
+DROP TABLE IF EXISTS drink_type;
+DROP TABLE IF EXISTS manufacturer;
+DROP TABLE IF EXISTS manufac_location;
+
+
 CREATE TABLE manufac_location(
     location_id INT UNSIGNED,
-    location_street_address VARCHAR(50)
-    location_city VARCHAR(50)
-    location_state_province VARCHAR(50)
-    location_country VARCHAR(50)
-    location_postal_address VARCHAR(20)
+    location_street_address VARCHAR(50) NOT NULL,
+    location_city VARCHAR(50) NOT NULL,
+    location_state_province VARCHAR(50) NOT NULL,
+    location_country VARCHAR(50) NOT NULL,
+    location_postal_address VARCHAR(20) NOT NULL,
     PRIMARY KEY (location_id)
 );
 
@@ -14,13 +21,13 @@ CREATE TABLE manufacturer(
     location_id INT UNSIGNED NOT NULL,
     net_worth INT UNSIGNED NOT NULL,
     PRIMARY KEY (manufac_id),
-    FOREIGN KEY (manufac_location_id) REFERENCES manufac_location (location_id),
+    FOREIGN KEY (manufac_location_id) REFERENCES manufac_location (location_id)
 );
 
-CREATE TABLE soda_type(
-    soda_type_id INT UNSIGNED,
-    soda_type_name VARCHAR(50) NOT NULL, 
-    PRIMARY KEY (soda_type_id)
+CREATE TABLE drink_type(
+    drink_type_id INT UNSIGNED,
+    drink_type_name VARCHAR(50) NOT NULL, 
+    PRIMARY KEY (drink_type_id)
 );
 
 CREATE TABLE flavor(
@@ -37,10 +44,10 @@ CREATE TABLE drink(
     sugar_content INT UNSIGNED NOT NULL,
     sodium_content INT UNSIGNED NOT NULL,
     serving_size INT UNSIGNED NOT NULL,
-    soda_type_id INT UNSIGNED NOT NULL,
+    drink_type_id INT UNSIGNED NOT NULL,
     flavor_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (drink_id),
     FOREIGN KEY (manufac_id) REFERENCES manufacturer (manufac_id),
-    FOREIGN KEY (soda_type_id) REFERENCES soda_type (soda_type_id),
+    FOREIGN KEY (drink_type_id) REFERENCES drink_type (drink_type_id),
     FOREIGN KEY (flavor_id) REFERENCES flavor (flavor_id)
 );
