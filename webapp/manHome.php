e63fbbe8bc044f86bd2f88e837af36e5ffeb8732
ttiedt <ttiedt@zagmail.gonzaga.edu>
2019-11-23 15:12:17 -0800

    File structure

---
+++ webapp/manHome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>CPSC 321</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <div id="container">
+    <?php
+
+    ?>
+  </div>
+</body>
+
+</html>
