182dda2548ead03b04b521a44964b6a44723421f
ttiedt <ttiedt@zagmail.gonzaga.edu>
2019-11-23 16:23:14 -0800

    login page

--- webapp/login.php
+++ webapp/login.php
@@ -8,8 +8,14 @@
 
 <body>
   <div id="container">
+    <h1>Login</h1>
+    <form action='login.php' method='POST'>
+      <p>Username</p>
+      <input type='text' name='username'/>
+      <p>Password</p>
+      <input type='password' name='password'/>
+    </form>
     <?php
-
     ?>
   </div>
 </body>
