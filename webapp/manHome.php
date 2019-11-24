e263dcae91d5e5bf54881cad98848c29a43ab1f2
zmckee2 <zach.mckee2@outlook.com>
2019-11-23 16:05:54 -0800

    User and manufacturer home

--- webapp/manHome.php
+++ webapp/manHome.php
@@ -8,6 +8,23 @@
 
 <body>
   <div id="container">
+  <h2> WELCOME MANUFACTURER </h2>
+    <div>
+      <div style="float:left; width:40%;">
+        <form action="add.php" style="margin:10px;">
+          <input type="submit" value="Enter Soda"/>
+        </form>
+        <form action="remove.php" style="margin:10px;">
+          <input type="submit" value="Remove Soda"/>
+        </form>
+        <form action="search.php" style="margin:10px;">
+          <input type="submit" value="Search Sodas"/>
+        </form>
+      </div>
+      <div style="float:left; width:40%; border:black; border-width:3px; border-style:solid; padding:3px;">
+        <p> All manufacture sodas </p>
+      </div>
+    </div>
     <?php
 
     ?>
--- webapp/userHome.php
+++ webapp/userHome.php
@@ -8,6 +8,23 @@
 
 <body>
   <div id="container">
+    <h2> WELCOME USER </h2>
+    <div>
+      <div style="float:left; width:40%;">
+        <form action="favorites.php" style="margin:10px;">
+          <input type="submit" value="Your Favorites"/>
+        </form>
+        <form action="search.php" style="margin:10px;">
+          <input type="submit" value="Search Sodas"/>
+        </form>
+        <form action="search.php" style="margin:10px;">
+          <input type="submit" value="Search Manufacturers"/>
+        </form>
+      </div>
+      <div style="float:left; width:40%; border:black; border-width:3px; border-style:solid; padding:3px;">
+        <p> Favorite soda result here </p>
+      </div>
+    </div>
     <?php
 
     ?>
