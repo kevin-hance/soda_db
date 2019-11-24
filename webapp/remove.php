f81acafaff25555bea372b396361e025cc0af55a
ttiedt <ttiedt@zagmail.gonzaga.edu>
2019-11-23 16:14:51 -0800

    Sux it zakc

--- webapp/remove.php
+++ webapp/remove.php
@@ -8,6 +8,13 @@
 
 <body>
   <div id="container">
+    <h1>Remove Soda</h1>
+    <form action='remove.php' method='POST'>
+      <p>Enter soda to remove: </p>
+      <input name='name' type='text'>
+      </br></br>
+      <input type='submit' value='Submit'/>
+    </form>
     <?php
 
     ?>
