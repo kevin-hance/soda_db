8cbbc7b02b34137547c107a44908f769efc0332f
[Jackson Paris] <[jparis@zagmail.gonzaga.edu]>
2019-12-02 19:30:02 -0800

    love

--- webapp/remove.php
+++ webapp/remove.php
@@ -1,3 +1,6 @@
+<?php
+ session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,13 +13,43 @@
   <div id="container">
     <h1>Remove Soda</h1>
     <form action='remove.php' method='POST'>
-      <p>Enter soda to remove: </p>
-      <input name='name' type='text'>
+      <p>Enter soda name to remove: </p>
+      <input name='remove' type='text'>
       </br></br>
-      <input type='submit' value='Submit'/>
+      <input type='submit' value='Submit' name ='submit'/>
     </form>
     <?php
-
+       include('database.php');
+       if(isset($_POST['submit']) && isset($_POST['remove'])){
+         $remove = $_POST['remove'];
+         $query1 = "SELECT drink_name FROM drink WHERE drink_name = '$remove'";
+         $dbRecords = mysql_query($query1,$dbLocalhost) or die("Problem reading table: ".mysql_error());
+         $record = mysql_fetch_row($dbRecords);
+         $deleteDrink = $record[0];
+         if(empty($deleteDrink)){
+          echo '<script language="javascript">';
+          echo 'alert("Drink does not exist in database")';
+          echo '</script>';
+         }
+         else{
+          $query2 = "SELECT drink_id FROM drink WHERE drink_name = '$remove'";
+          $dbRecords = mysql_query($query2,$dbLocalhost) or die("Problem reading table: ".mysql_error());
+          $record = mysql_fetch_row($dbRecords);
+          $deleteDrinkId = $record[0];
+          $query3 = "DELETE FROM favorite WHERE drink_id = '$deleteDrinkId'";
+          mysql_query($query3,$dbLocalhost) or die("Problem reading table: ".mysql_error());
+          $query4 = "DELETE FROM drink WHERE drink_id = '$deleteDrinkId'";
+          mysql_query($query4,$dbLocalhost) or die("Problem reading table: ".mysql_error());
+          echo '<script language="javascript">';
+          echo 'alert("Drink deleted")';
+          echo '</script>';
+          header('Location: manHome.php');
+          exit();
+         }
+         
+       }
+   
+       mysql_close($dbLocalhost);
     ?>
   </div>
 </body>
